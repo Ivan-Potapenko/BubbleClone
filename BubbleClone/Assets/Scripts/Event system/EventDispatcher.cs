9b140e762540b87338c906db233a2cadbd76795e
1one <van.van123@list.ru>
2021-07-10 00:13:08 +0200

    Event system added

---
+++ BubbleClone/Assets/Scripts/Event system/EventDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Events
+{
+
+    public class EventDispatcher : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Event _someEvent;
+
+        public void Dispatch()
+        {
+            _someEvent.Dispatch();
+        }
+
+    }
+}
+
