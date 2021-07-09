c828720f3ff69b3d1eda6de1174ba0a431b307df
1one <van.van123@list.ru>
2021-07-10 00:13:43 +0200

    PlayerBubble script create

---
+++ BubbleClone/Assets/Scripts/Game/Player/PlayerBubble.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class PlayerBubble : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed;
+        
+
+        void Start()
+        {
+
+        }
+
+
+    }
+
+}
