0138055c49536c03950315e3e298b6677acf6ee4
1one <van.van123@list.ru>
2021-07-10 00:14:08 +0200

    PlayerBubbleControl script added

---
+++ BubbleClone/Assets/Scripts/Game/Player/PlayerBubbleControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Events;
+
+
+
+namespace Game
+{
+    public class PlayerBubbleControl : MonoBehaviour
+    {
+        [SerializeField]
+        private EventDispatcher _mooveEventDispatcher; 
+
+
+        void Start()
+        {
+
+        }
+
+    }
+}
+
