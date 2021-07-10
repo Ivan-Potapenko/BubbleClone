63c9b5d310c4f49395fab2a0e86bc31de0633d94
1one <van.van123@list.ru>
2021-07-11 00:40:24 +0200

    Geyser creted

---
+++ BubbleClone/Assets/Scripts/Game/Environment/Geyser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geyser : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 _direction;
+
+    [SerializeField]
+    private float GeyserForce;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var _collisionGameObjectRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (_collisionGameObjectRigidbody != null)
+        {
+            _collisionGameObjectRigidbody.AddForce(_direction * GeyserForce, ForceMode2D.Impulse);
+        }
+    }
+
+}
