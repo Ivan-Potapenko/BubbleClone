96b4f593cb7f4a3433317f5ba27da45faa7df2b9
1one <van.van123@list.ru>
2021-07-10 15:49:53 +0200

    Vector2ScriptableObject added

---
+++ BubbleClone/Assets/Scripts/ScriptableObjects/Vector2ScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewVector2", menuName = "Vector2")]
+    public class Vector2ScriptableObject : ScriptableObject
+    {
+        public Vector2 value;
+    }
+}
+
