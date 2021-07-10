fb8892e48db11ed05a8c8e3bda6deb052567a572
1one <van.van123@list.ru>
2021-07-11 00:38:25 +0200

    added key

---
+++ BubbleClone/Assets/Scripts/Game/Environment/Key.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Key : MonoBehaviour
+    {
+        [SerializeField]
+        private Door _door;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.gameObject.tag == "Player")
+            {
+                GetKey();
+                Destroy(gameObject);
+            }
+        }
+
+        private void GetKey()
+        {
+            _door.OpenDoor();
+        }
+        
+
+    }
+}
+
