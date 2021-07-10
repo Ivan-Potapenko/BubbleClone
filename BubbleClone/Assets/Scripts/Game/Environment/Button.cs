89d203909584f4dc215fff8c9790be9aa5c0afa1
1one <van.van123@list.ru>
2021-07-11 00:06:03 +0200

    added Door and Button

---
+++ BubbleClone/Assets/Scripts/Game/Environment/Button.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Button : MonoBehaviour
+    {
+        [SerializeField]
+        private Door _door;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.gameObject.tag == "Player")
+            {
+                ButtonDown();
+            }
+        }
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player" && !_door.IsOpen)
+            {
+                ButtonDown();
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                ButtonUp();
+            }
+        }
+
+        private void ButtonDown()
+        {
+            _door.OpenDoor();
+        }
+        private void ButtonUp()
+        {
+            _door.CloseDoor();
+        }
+
+        
+
+    }
+}
---
+++ BubbleClone/Assets/Scripts/Game/Environment/Door.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class Door : MonoBehaviour
+    {
+
+        private bool _isOpen = false;
+
+        public bool IsOpen { get => _isOpen; set => _isOpen = value; }
+
+        public void OpenDoor()
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+            gameObject.GetComponent<Collider2D>().isTrigger = true;
+            _isOpen = true;
+        }
+
+        public void CloseDoor()
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            gameObject.GetComponent<Collider2D>().isTrigger = false;
+            _isOpen = false;
+        }
+    }
+}
+
