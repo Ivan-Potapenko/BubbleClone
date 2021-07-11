5385c81cdd4e3ffaaf757bd3262f4d29114cc06b
Ivan Potapenko <potapenko@panzerdog.com>
2021-07-11 14:44:38 +0200

    Settings scriptable object added

---
+++ BubbleClone/Assets/Scripts/ScriptableObjects/Settings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    
+    [CreateAssetMenu(fileName = "Settings", menuName = "Settings")]
+    public class Settings : ScriptableObject
+    {
+        public float MusicVolume;
+        public float SoundVolume;
+    }
+
+}
