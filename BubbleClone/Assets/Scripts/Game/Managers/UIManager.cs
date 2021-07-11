f58883acb2383a2d55eb829b72154b70695c629c
1one <van.van123@list.ru>
2021-07-11 02:10:03 +0200

    Sounds Build Library

---
+++ BubbleClone/Assets/Scripts/Game/Managers/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _playButton;
+
+        [SerializeField]
+        private Button _settingsButton;
+
+        [SerializeField]
+        private Button _exitButton;
+
+        private void Start()
+        {
+            
+        }
+    }
+
+}
