4014596df9ba549f7e19d0de0ae9e4475208e50e
Ivan Potapenko <potapenko@panzerdog.com>
2021-07-11 14:45:02 +0200

    UI manager added

--- BubbleClone/Assets/Scripts/Game/Managers/UIManager.cs
+++ BubbleClone/Assets/Scripts/Game/Managers/UIManager.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using Events;
+using UnityEngine.SceneManagement;
 
 namespace UI
 {
     public class UIManager : MonoBehaviour
     {
+        public static UIManager Instance;
+
+
         [SerializeField]
-        private Button _playButton;
+        private Button _continueButton;
+
+        [SerializeField]
+        private Button _newGameButton;
 
         [SerializeField]
         private Button _settingsButton;
@@ -14,36 +22,122 @@ namespace UI
         [SerializeField]
         private Button _exitButton;
 
+        [SerializeField]
+        private EventListener _nextLevelEventListner;
+
+        [SerializeField]
+        private GameObject _menu;
+
+        [SerializeField]
+        private GameObject _settings;
+
+        [SerializeField]
+        private GameObject _game;
+
+        [SerializeField]
+        private GameObject _pause;
+
+        [SerializeField]
+        private GameObject _updateManager;
+
+        private int _savedLevelNum = 1;
+
+        private string _filePath;
+
+        private const string LEVEL_NUM = "LEVEL_NUM";
+
+
+
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
         private void OnEnable()
         {
-            _playButton.onClick.AddListener(OnPlayButton);
+            _continueButton.onClick.AddListener(OnContinueButton);
+
+            _newGameButton.onClick.AddListener(OnNewGameButton);
             _settingsButton.onClick.AddListener(OnSettingsButton);
             _exitButton.onClick.AddListener(OnExitButton);
+            _nextLevelEventListner.ActionsToDo += NextLevel;
+
         }
 
         private void OnDisable()
         {
-            _playButton.onClick.RemoveListener(OnPlayButton);
+            _continueButton.onClick.RemoveListener(OnContinueButton);
+            _newGameButton.onClick.RemoveListener(OnNewGameButton);
             _settingsButton.onClick.RemoveListener(OnSettingsButton);
             _exitButton.onClick.RemoveListener(OnExitButton);
+            _nextLevelEventListner.ActionsToDo -= NextLevel;
         }
 
-
-        private void OnPlayButton()
+        private void OnContinueButton()
         {
+            if (PlayerPrefs.HasKey(LEVEL_NUM))
+            {
+                _savedLevelNum = PlayerPrefs.GetInt(LEVEL_NUM);
+                LoadLevel();
+            }
+            else
+            {
+                OnNewGameButton();
+                return;
+            }
+        }
 
+        private void NextLevel()
+        {
+            _savedLevelNum++;
+            Save();
+            LoadLevel();
+        }
+
+        private void Save()
+        {
+            PlayerPrefs.SetInt(LEVEL_NUM, _savedLevelNum);
+        }
+
+        private void OnNewGameButton()
+        {
+            PlayerPrefs.DeleteKey(LEVEL_NUM);
+            _savedLevelNum = 1;
+            LoadLevel();
+        }
+
+        private void LoadLevel()
+        {
+            TurnOffAllElements();
+            _game.SetActive(true);
+            SceneManager.LoadScene(_savedLevelNum);
         }
 
         private void OnExitButton()
         {
-
+            Application.Quit();
         }
 
         private void OnSettingsButton()
         {
 
         }
-            
+
+        private void TurnOffAllElements()
+        {
+            _menu.SetActive(false);
+            _game.SetActive(false);
+            _settings.SetActive(false);
+        }
+
     }
 
 }
