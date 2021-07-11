74ba97a3f56ed9a445a19235cadc5f3fae5990ad
Ivan Potapenko <potapenko@panzerdog.com>
2021-07-11 14:47:29 +0200

    Level Finish

---
+++ BubbleClone/Assets/Scripts/Game/Environment/Finish.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Events;
+
+
+namespace Game
+{
+    public class Finish : MonoBehaviour
+    {
+        [SerializeField]
+        private EventDispatcher _nextLevelEventDispatcher;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.gameObject.tag == "Player")
+            {
+                _nextLevelEventDispatcher.Dispatch();
+            }
+        }
+
+    }
+}
+
